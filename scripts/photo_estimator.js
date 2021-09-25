122a37b23cd2fe46fda6d0348f0a2a59edf41e79
Karl <KarltroidContact@Gmail.com>
2021-09-25 04:04:22 -0400

    Added CMYK color data

--- scripts/photo_estimator.js
+++ scripts/photo_estimator.js
@@ -4,12 +4,12 @@ window.addEventListener('load', function()
 	imageDisplay = document.getElementById('userimage');
 	imageCanvas = document.getElementById('userimage-canvas');
 	resolutionDisplay = document.getElementById('info-resolution');
-	aspectratioDisplay = document.getElementById('info-aspectratio');
-	colorratioDisplay = document.getElementById('info-colorratio');
-	averagehexvalueDisplay = document.getElementById('info-averagehexvalue');
-	ignoredpixelsDisplay = document.getElementById('info-ignoredpixels');
+	aspectRatioDisplay = document.getElementById('info-aspectratio');
+	averageRgbDisplay = document.getElementById('info-averagergb');
+	averageHexValueDisplay = document.getElementById('info-averagehexvalue');
+	averageCmykDisplay = document.getElementById('info-averagecmyk');
+	ignoredPixelsDisplay = document.getElementById('info-ignoredpixels');
 	luminanceDisplay = document.getElementById('info-luminance');
-	context = imageCanvas.getContext('2d');
 });
 
 function getImageData()
@@ -22,18 +22,19 @@ function getImageData()
 
 		image.onerror = function()
 		{
+			// alert the user that their image file is not supported
 			alert("not a valid file: " + file.type);
 			return;
 		};
 
 		image.onload = function()
 		{
-			// load canvas
+			// prepare canvas for drawing
 			imageCanvas.width = this.width;
 			imageCanvas.height = this.height;
 
-			// get color data
-			context = imageCanvas.getContext('2d');
+			// draw image to canvas and get color data
+			var context = imageCanvas.getContext('2d');
 			context.drawImage(image, 0, 0);
 			var colorData = context.getImageData(0, 0, this.width-1, this.height-1);
 
@@ -56,47 +57,55 @@ function displayImageData(image, colorData)
 	// display aspect ratio
 	var aspectDivisor = gcd(image.width, image.height);
 	if (aspectDivisor > 1) // display simplified aspect ratio
-		aspectratioDisplay.innerHTML = (image.width / aspectDivisor) + ":" + (image.height / aspectDivisor);
+		aspectRatioDisplay.innerHTML = (image.width / aspectDivisor) + ":" + (image.height / aspectDivisor);
 	else
-		aspectratioDisplay.innerHTML = ("<font color=#823333>Nonstandard Ratio </font>" + (image.width / image.height).toFixed(3));
+		aspectRatioDisplay.innerHTML = ("<font color=#823333>Custom Ratio </font>" + (image.width / image.height).toFixed(3));
 
 	// add up all RGB values and count white/transparent pixels that won't be printed
-	var totalRed = 0, totalGreen = 0, totalBlue = 0, ignoredPixels = 0;
+	var rgb = [0, 0, 0], transparentPixels = 0, whitePixels = 0;
 	for (var i = 0; i < colorData.data.length; i += 4)
 	{
 		if (colorData.data[i+3] == 0)
 		{
-			ignoredPixels++;
+			transparentPixels++;
 			continue;
 		}
-		else if (colorData.data[i] == 255 &&colorData.data[i+1] == 255 && colorData.data[i+2] == 255)
-			ignoredPixels++;
+		else if (colorData.data[i] == 255 && colorData.data[i+1] == 255 && colorData.data[i+2] == 255)
+			whitePixels++;
 
-		totalRed += colorData.data[i];
-		totalGreen += colorData.data[i+1];
-		totalBlue += colorData.data[i+2];
+		rgb[0] += colorData.data[i];
+		rgb[1] += colorData.data[i+1];
+		rgb[2] += colorData.data[i+2];
 	}
 
 	// get average rgb color of the image
 	var pixelAmount = colorData.data.length / 4;
-	var countedPixels = pixelAmount - ignoredPixels;
-	var averageRed = Math.round(totalRed/countedPixels);
-	var averageBlue = Math.round(totalBlue/countedPixels);
-	var averageGreen = Math.round(totalGreen/countedPixels);
+	var countedPixels = pixelAmount - transparentPixels;
+	rgb = [ Math.round(rgb[0] / countedPixels),
+					Math.round(rgb[1] / countedPixels),
+					Math.round(rgb[2] / countedPixels) ];
 
 	// display average rgb color of the image
-	colorratioDisplay.innerHTML = "<font color=red>" + averageRed +
-						"</font> : <font color=green>" + averageBlue +
-						"</font> : <font color=blue>" + averageGreen + "</font>";
+	averageRgbDisplay.innerHTML =	 "<font color=red>" + rgb[0] + "</font> : \
+																	<font color=green>" + rgb[1] + "</font> : \
+																	<font color=blue>" + rgb[2] + "</font>";
+
+	// convert rgb to cmyk and display it
+	var cmyk = rgbToCmyk(rgb[0], rgb[1], rgb[2]);
+	averageCmykDisplay.innerHTML = "<font color=#0093d3>" + cmyk[0] + "</font>, \
+																	<font color=#cc006b>" + cmyk[1] + "</font>, \
+																	<font color=#bab109>" + cmyk[2] + "</font>, "
+																	+ cmyk[3];
 
 	// convert rgb value to hex and display it
-	averagehexvalueDisplay.innerHTML = rgbToHex(averageRed, averageBlue, averageGreen);
+	var hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
+	averageHexValueDisplay.innerHTML = hex;
 
 	// calculate and display the % of pixels that will be ignored during printing
-	ignoredpixelsDisplay.innerHTML = (ignoredPixels/pixelAmount * 100).toFixed(2) + "%";
+	ignoredPixelsDisplay.innerHTML = ((whitePixels + transparentPixels) / pixelAmount * 100).toFixed(2) + "%";
 
 	// convert rgb to luminance and display it
-	luminanceDisplay.innerHTML = (((0.2126 * averageRed) + (0.7152 * averageGreen) + (0.0722 * averageBlue)) / 255 * 100).toFixed(2) + "%";
+	luminanceDisplay.innerHTML = (((0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2])) / 255 * 100).toFixed(2) + "%";
 }
 
 function gcd(a, b)
@@ -113,4 +122,29 @@ function componentToHex(c)
 function rgbToHex(r, g, b)
 {
 	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function rgbToCmyk(r, g, b)
+{
+    if ((r == 0) && (g == 0) && (b == 0))
+        return ["0%", "0%", "0%", "100%"];
+    else
+		{
+        var calcR = 1 - (r / 255),
+            calcG = 1 - (g / 255),
+            calcB = 1 - (b / 255);
+
+        var k = Math.min(calcR, Math.min(calcG, calcB)),
+            c = (calcR - k) / (1 - k),
+            m = (calcG - k) / (1 - k),
+            y = (calcB - k) / (1 - k);
+
+				// convert to percentage
+				c = Math.round(c * 100) + "%";
+				m = Math.round(m * 100) + "%";
+				y = Math.round(y * 100) + "%";
+				k = Math.round(k * 100) + "%";
+
+        return [c, m, y, k];
+    }
 }
